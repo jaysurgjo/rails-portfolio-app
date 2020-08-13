78d9a1077d183a5b8abd99952d297623fb7d4e69
github username <github email address>
2020-08-13 15:46:05 -0400

    added structure to appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 end
 
-puts "Hello World"
+  def index
+    @task = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+  end
+
+
+  def update
+
+  end
+
+
+  def destroy
+  end
