1f333a28150e5865c3cf68f9d142bb5ef0a59435
github username <github email address>
2020-12-23 03:46:20 -0500

    added migration add admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201219175726) do
+ActiveRecord::Schema.define(version: 20201223084311) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20201219175726) do
     t.datetime "updated_at", null: false
     t.boolean "email_confirmed"
     t.string "confirm_token"
+    t.boolean "admin", default: false
   end
 
 end
